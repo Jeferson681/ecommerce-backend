f0c86057d1502f8cc875a560b454caa84ed5e3cd
jeferson681 <Jefersonoliveiradesousa681@gmail.com>
2026-05-18 11:15:22 -0300

    fix ci and mypy config

--- app/api/routers/user.py
+++ app/api/routers/user.py
@@ -15,12 +15,14 @@ from app.modules.user.schemas import (
     UserRead,
     UserUpdate,
 )
-from app.modules.user.use_cases import change_password as change_user_password
-from app.modules.user.use_cases import create_user as create_user_use_case
-from app.modules.user.use_cases import delete_user as delete_user_use_case
-from app.modules.user.use_cases import get_user as get_user_use_case
-from app.modules.user.use_cases import list_users as list_users_use_case
-from app.modules.user.use_cases import update_user as update_user_use_case
+from app.modules.user.use_cases import (
+    change_password as change_user_password,
+    create_user as create_user_use_case,
+    delete_user as delete_user_use_case,
+    get_user as get_user_use_case,
+    list_users as list_users_use_case,
+    update_user as update_user_use_case,
+)
 
 router = APIRouter(prefix="/users", tags=["users"])
 
